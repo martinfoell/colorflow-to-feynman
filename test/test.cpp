116def044abafea9526374b2c84eedea5f3d68cc
martinfoell <m.foell.1999@gmail.com>
2023-09-13 00:52:00 +0200

    made vector of quarks

--- test/test.cpp
+++ test/test.cpp
@@ -6,33 +6,52 @@ using namespace arma;
 
 int main()
   {
-    std::vector<std::string> in = {"q1", "Q1"};
+    std::vector<std::string> in = {"q1", "Q2"};
     std::vector<std::string> inter= {"g"};
-    std::vector<std::string> out= {"q2", "Q2"};
+    std::vector<std::string> out= {"q3", "Q4"};
     
     int n = in.size()+inter.size()+out.size();    
     std::cout << "n = " << n << std::endl;
     // Create a new vector to store the concatenated strings
-    std::vector<std::string> cf1 = {"q1", "g", "Q1"};
-    std::vector<std::string> cf2 = {"Q2", "g", "q2"};
+    std::vector<std::string> cf1 = {"q1", "g", "Q2"};
+    std::vector<std::string> cf2 = {"Q4", "g", "q3"};
 
     std::vector<std::string> particles;
     particles.reserve( in.size() + inter.size() + out.size() ); // preallocate memory
     particles.insert( particles.end(), in.begin(), in.end() );
     particles.insert( particles.end(), inter.begin(), inter.end() );
     particles.insert( particles.end(), out.begin(), out.end() );
+
+    // std::vector<std::string> quarks;
+
+    std::vector<std::string> quarks;
+
+    // Iterate through the vector and extract the first character from each string
+    for (const std::string& str : particles) {
+        if (!str.empty()) { // Make sure the string is not empty
+	  if(str[0] == 'q' || 'Q'){
+            quarks.push_back(str);}
+        }
+    }
+
+    // Print the first characters
+    std::cout << "First characters:" << std::endl;
+    for (string c : quarks) {
+        std::cout << c << " ";
+    }
+    std::cout << std::endl;
     
     std::map<std::string, int> particle_index;
     for (size_t i = 0; i < particles.size(); ++i) {
       particle_index[particles[i]] = i;
     }
 
-
-    std::cout << "q1: " << particle_index["q1"] << std::endl;
-    std::cout << "Q1: " << particle_index["Q1"] << std::endl;
-    std::cout << "g: " << particle_index["g"] << std::endl;
-    std::cout << "q2: " << particle_index["q2"] << std::endl;
-    std::cout << "Q2: " << particle_index["Q2"] << std::endl;
+    std::cout << "particle_index[\"q1\"] = " << particle_index["q1"] << std::endl;
+    std::cout << "particle_index[\"g\"] = " << particle_index["g"] << std::endl;
+    std::cout << "particle_index[\"Q2\"] = " << particle_index["Q2"] << std::endl;
+    std::cout << "particle_index[\"q3\"] = " << particle_index["q3"] << std::endl;
+    std::cout << "particle_index[\"Q4\"] = " << particle_index["Q4"] << std::endl;
+    
 
     arma::imat A = arma::imat(n+1, n+1); //Initialize matrix but don't fill.
     
